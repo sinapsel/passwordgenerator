8082a3a33bd2fea5fa87d7ed47ce0a3cb620ca8c
Sinapsel <marksimanovskiy@gmail.com>
2017-01-07 21:16:21 +0300

    Passgen

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo -e "\n\n\n\n\n ###########################################"
+echo -e "\n\n\tInstalltion passgen"
+echo -e "\tIn this case installation process is just making passgen executable and moving it to /bin/"
+echo -e "\nThen you can run passgen in terminal like vim or firefox"
+
+chmod +x passgen
+sudo cp passgen /bin/
+
+echo -e "\n\n\tDone......"
+echo -e "\n ########################################### \n\n\n\n\n"
